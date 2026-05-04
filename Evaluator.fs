01007e8f2ccfbde81cb3209506a2782de052bb6f
yuchan <c3686336@gmail.com>
2026-05-04 22:21:36 +0900

    fix: Fix duplicate parsing

--- Evaluator.fs
+++ Evaluator.fs
@@ -2,23 +2,32 @@ module Evaluator
 
 open FSharp.Collections
 
-type Tile = int
+type Tile =
+  | Tile of int
+
+  override this.ToString() =
+    let (Tile v) = this
+    [|"Wat";"🀐";"🀑";"🀒";"🀓";"🀔";"🀕";"🀖";"🀗";"🀘"|][v]
+
 type Kantsu = Tile
 type Shuntsu = Tile * Tile * Tile
 type Kotsu = Tile
 type Toitsu = Tile
-type ListHand = (int * int) list // Tile's name and count
+type ListHand = (Tile * int) list // Tile's name and count
 type ArrayHand = int array
-type Hand = ArrayHand * Kantsu list
+
+type Hand =
+  ArrayHand * Kantsu list
+
 type ParsedHand = Kantsu list * Shuntsu list * Kotsu list * Toitsu
 
-let TrimHead = List.skipWhile (snd >> (=) 0) 
+let FilterZero = List.filter (snd >> (<>) 0) 
 
-let TryParseNTile n hand =
+let TryParseNTile (n: int) (hand: ListHand): (Tile * ListHand) option =
   match hand with
     | (t, c) :: xs when c >= n ->
       (t, c - n) :: xs
-        |> TrimHead
+        |> FilterZero
         |> fun x -> t, x
         |> Some
     | _ -> None
@@ -26,19 +35,20 @@ let TryParseNTile n hand =
 let TryParseToitsu = TryParseNTile 2
 let TryParseKotsu = TryParseNTile 3
 
-let TryParseShuntsu hand =
+let TryParseShuntsu (hand: ListHand): (Shuntsu * ListHand) option =
   match hand with
-    | (a, a1) :: (b, a2) :: (c, a3) :: xs when a1 > 0 && a2 > 0 && a3 > 0 && b = a + 1 && c = a + 2 ->
-      (a, a1 - 1) :: (b, a2 - 1) :: (c, a3 - 1) :: xs
-        |> TrimHead
-        |> fun x -> (a, b, c), x
+    | (Tile a, a1) :: (Tile b, a2) :: (Tile c, a3) :: xs when a1 > 0 && a2 > 0 && a3 > 0 && b = a + 1 && c = a + 2 ->
+      (Tile a, a1 - 1) :: (Tile b, a2 - 1) :: (Tile c, a3 - 1) :: xs
+        |> FilterZero
+        |> fun x -> (Tile a, Tile b, Tile c), x
         |> Some
     | _ -> None
 
-let rec TryParseHeadlessHandAsMuch (hand: ListHand) (parsedHand: ParsedHand) =
+let rec TryParseHeadlessHandAsMuch (hand: ListHand) (parsedHand: ParsedHand): ParsedHand list =
   match hand with
     | [] -> [parsedHand]
     | hand ->
+      // Order of Shuntsu and Kotsu shoudn't matter
       let (parsedKantsu, parsedShuntsu, parsedKotsu, parsedToitsu) = parsedHand
       let shuntsu = TryParseShuntsu hand
       let kotsu = TryParseKotsu hand
@@ -52,19 +62,19 @@ let rec TryParseHeadlessHandAsMuch (hand: ListHand) (parsedHand: ParsedHand) =
         |> Option.map (fun (newKotsu, hand) -> TryParseHeadlessHandAsMuch hand (parsedKantsu, parsedShuntsu, newKotsu::parsedKotsu, parsedToitsu))
         |> Option.defaultValue []
 
-      shuntsuParsingResult @ kotsuParsingResult
+      shuntsuParsingResult @ kotsuParsingResult |> List.distinct
 
-let ArraytoList (hand: ArrayHand) =
-  List.fold (fun acc elem -> if hand[elem] <> 0 then (elem, hand[elem])::acc else acc) [] [1..9] |> List.rev
+let ArraytoList (hand: ArrayHand): ListHand =
+  List.fold (fun acc elem -> if hand[elem] <> 0 then (Tile elem, hand[elem])::acc else acc) [] [1..9] |> List.rev
 
-let TryParseHeadlessHand (hand: ArrayHand) (toitsu) (kantsu: Kantsu list) =
+let TryParseHeadlessHand (hand: ArrayHand) (toitsu: Toitsu) (kantsu: Kantsu list): ParsedHand list =
   let result = TryParseHeadlessHandAsMuch (ArraytoList hand) (kantsu, [], [], toitsu)
   List.filter (fun (kan: Kantsu list, shun: Shuntsu list, ko: Kotsu list, toi: Toitsu) -> kan.Length + shun.Length + ko.Length = 4) result
 
-let TryParse ((hand, kantsu): Hand) =
+let TryParse ((hand, kantsu): Hand): ParsedHand list =
   List.fold (fun acc elem ->
              if hand[elem] >= 2 then
-               (TryParseHeadlessHand (Array.updateAt elem (hand[elem] - 2) hand) elem kantsu)::acc
+               (TryParseHeadlessHand (Array.updateAt elem (hand[elem] - 2) hand) (Tile elem) kantsu) :: acc
              else
                acc) [] [1..9]
-    |> List.filter ((<>) [])
+    |> List.filter ((<>) []) |> List.concat
--- Program.fs
+++ Program.fs
@@ -4,9 +4,17 @@ open Evaluator;
 
 [<EntryPoint>]
 let main argv =
-  let result = TryParse ([|0;3;1;2;2;1;1;1;1;0|], [9])
-  // let result = TryParseHeadlessHandAsMuch 
-  
-  printfn "%A" result |> ignore
+  let result = TryParse ([|0;3;1;1;2;1;1;1;1;3|], [])
+  printfn $"{result}" |> ignore
+  let result = TryParse ([|0;4;1;1;1;1;1;1;1;3|], [])
+  printfn $"{result}" |> ignore
+  let result = TryParse ([|0;3;1;4;2;1;1;1;1;3|], [])
+  printfn $"{result}" |> ignore
+  let result = TryParse ([|0;3;1;1;1;1;1;1;1;4|], [])
+  printfn $"{result}" |> ignore
+
+  // let result = TryParseHeadlessHandAsMuch [(Tile 1, 4); (Tile 2, 1); (Tile 3, 1); (Tile 4, 1); (Tile 5, 1); (Tile 6, 1); (Tile 7, 1); (Tile 8, 1); (Tile 9, 1)] ([], [], [], Tile 9)
+  let result = TryParseHeadlessHandAsMuch [(Tile 1, 4); (Tile 2, 1); (Tile 3, 1)] ([], [], [], Tile 9)
+  printfn $"{result}" |> ignore
 
   0
